5849a653504e2e5f601efd64e33a3510f3e003b8
TanmayBansal29 <bansaltanmay554@gmail.com>
2025-08-26 01:59:18 +0530

    add: Login Controller Phase 1

--- santrion-auth-service/src/controllers/auth.controller.js
+++ santrion-auth-service/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const OtpModel = require("../models/Otp.model")
 const UserProfile = require("../models/User.model")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const {registerValidationSchema, roleDomainMap} = require("../validations/registeration.validation")
 
 // Signup controller
@@ -118,3 +119,105 @@ exports.signup = async (req, res) => {
 }
 
 // Login Controller
+exports.login = async(req, res) => {
+    try {
+        // Get email/username --> identifier and password from req body
+        const {identifier, password} = req.body
+
+        // Checking if identifier or password is missing
+        if(!identifier || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Please Enter all the required details"
+            })
+        }
+
+        // Detect whether identifier is email or username
+        const isEmail = /\S+@\S+\.\S+/.test(identifier)
+
+        // Find the user with the provided username
+        const user = isEmail
+        ? await UserProfile.findOne({ email: identifier.toLowerCase().trim() })
+        : await UserProfile.findOne({ username: new RegExp(`^${identifier}$`, "i") });
+
+        // If user is not found in the database
+        if(!user) {
+            return res.status(400).json({
+                success: false,
+                message: "User not registered. Please register to continue"
+            })
+        }
+
+        // Check if account is locked
+        if(user.isLocked && user.lockUntil > Date.now()) {
+            return res.status(403).json({
+                success: false,
+                message: "Account locked due to multiple failed login attempts. Try again later"
+            })
+        }
+
+        // Validate Password
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        // Generate JWT Token and compare password
+        if(!isPasswordValid) {
+            user.failedLoginAttempts = (user.failedLoginAttempts || 0) + 1;
+            user.lastFailedLoginAt = new Date();
+
+            // Lock account if too many failed attempts
+            if (user.failedLoginAttempts >= 5){
+                user.isLocked = true;
+                user.lockUntil = Date.now() + 15 * 60 * 1000
+            }
+
+            await user.save();
+
+            return res.status(400).json({
+                success: false,
+                message: "Invalid username or password"
+            })
+        }
+
+        // Reset failed attempts
+        user.failedLoginAttempts = 0;
+        user.isLocked = false;
+        user.lockUntil = undefined;
+
+        // Track login info
+        user.lastLoginAt = new Date();
+        user.lastLoginIP = req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+        user.loginCount = (user.loginCount || 0) + 1;
+
+        await user.save();
+
+        // Generate JWT
+        const token = jwt.sign(
+            {username: user.username, id:user._id, role: user.role},
+            process.env.JWT_SECRET,
+            { expiresIn: "24h"}
+        );
+
+        // Set Cookie for token and return success response
+        const options = {
+            expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict"
+        };
+
+        const {password: _, failedLoginAttempts, lockUntil, isLocked, ...safeUser} = user.toObject();
+        res.cookie("token", token, options).status(200).json({
+            success: true,
+            token,
+            data: safeUser,
+            message: "Login Successful"
+        });
+
+    } catch (error) {
+        console.error("Error Occurred Login: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Login Failure. Please try again"
+        })
+    }
+}
