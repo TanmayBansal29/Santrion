210b5b15c7ba325aecc33a8abbac47301b112e8b
Abhisharma1308 <as6402502@gmail.com>
2025-09-04 19:25:06 +0530

    add: changes in app.js

--- santrion-frontend-service/src/App.jsx
+++ santrion-frontend-service/src/App.jsx
@@ -1,15 +1,17 @@
 import SignUpForm from "./components/SignUpForm"
 import SignUpForm2 from "./components/SignUpForm2"
 import LoginPage from "./components/LoginPage"
-
+import SetPassword from "./components/SetPassword"
+import UpdatePassword from "./components/UpdatePassword"
 function App() {
 
   return (
     <>
   {/* <SignUpForm/> */}
-
-  <SignUpForm2/>
+  {/* <SignUpForm2/> */}
   {/* <LoginPage/> */}
+    {/* <SetPassword/> */}
+    <UpdatePassword/>
   </>
   )
 }
