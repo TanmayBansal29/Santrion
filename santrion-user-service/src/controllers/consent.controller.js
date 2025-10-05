094e3cfe9fdc7965441122fc56bed49a44a1a7d2
TanmayBansal29 <bansaltanmay554@gmail.com>
2025-10-05 20:12:16 +0530

    add: controller for users to give the consent

--- santrion-user-service/src/controllers/consent.controller.js
+++ santrion-user-service/src/controllers/consent.controller.js
@@ -1,3 +1,4 @@
+const ActivityLog = require("../models/ActivityLog.model")
 const { CONSENT_TYPES, Consent } = require("../models/Consent.model")
 
 // Controller to give the consent
@@ -33,6 +34,15 @@ exports.giveConsent = async (req,res) => {
             deviceInfo: req.headers['user-agent'],
             givenAt: status === "granted" ? new Date() : undefined,
             revokedAt: status === "revoked" ? new Date() : undefined
+        })
+
+        // Logging the activity
+        await ActivityLog.create({
+            userId,
+            type: "Consent",
+            description: `User given the consent: ${consentType}`,
+            ipAddress: req.ip,
+            deviceInfo: req.headers["user-agent"]
         })
 
         return res.status(200).json({
@@ -85,12 +95,62 @@ exports.getMyConsents = async (req, res) => {
                 latest: latestStatus // compact view
             }
         });
-        
+
     } catch (error) {
         console.error("Error fetching all the consents: ", error)
         return res.status(500).json({
             success: false,
             message: "Something went wrong fetching all the consents"
+        })
+    }
+}
+
+// Controller to update / withdraw the given consent
+exports.withdrawConsent = async (req, res) => {
+    try {
+        const userId = req.user.id // Extracted from middleware
+        const consentId = req.params
+
+        // Find the consent that belongs to user
+        const consent = await Consent.findOne({
+            _id: consentId,
+            userId,
+            status: "granted"
+        })
+
+        if(!consent) {
+            return res.status(404).json({
+                success: false,
+                message: "Consent not found or already withdrawn"
+            })
+        }
+
+        // Update status to revoked
+        consent.status = "revoked"
+        consent.revokedAt = new Date()
+
+        await consent.save()
+
+        // Logging the activity
+        await ActivityLog.create({
+            userId,
+            type: "Consent",
+            description: "User Withdrawn consent",
+            ipAddress: req.ip,
+            deviceInfo: req.headers["user-agent"]
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "Consent withdrawn successfully",
+            data: consent
+        })
+
+    } catch (error) {
+        console.error("Error while withdrawing the consent: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while withdrawing the consent"
         })
     }
 }
