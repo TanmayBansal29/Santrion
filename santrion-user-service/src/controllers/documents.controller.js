1fd16a66d92431ecf8875e69646a9a13c1a15b87
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-10-08 14:40:27 +0530

    add: controller to fetch all documents user and admin

--- santrion-user-service/src/controllers/documents.controller.js
+++ santrion-user-service/src/controllers/documents.controller.js
@@ -1,3 +1,5 @@
+const ActivityLog = require("../models/ActivityLog.model")
+const GeneralDocuments = require("../models/Documents.model")
 const CloudinaryService = require("../services/cloudinaryService")
 const streamifier = require("streamifier")
 
@@ -81,5 +83,124 @@ exports.uploadDocument = async (req, res) => {
             success: false,
             message: "Something went wrong uploading the document"
         })  
+    }
+}
+
+// Controller to get all my documents
+exports.getMyDocuments = async (req, res) => {
+    try {
+        const userId = req.user.id // extracted from auth middleware
+
+        // Pagination
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 10
+        const skip = (page - 1) * limit
+
+        // Filtering
+        const filters = { userId }
+        if(req.query.type) filters.type = req.query.type
+
+        // Sorting
+        const sortBy = req.query.sortBy || "createdAt"
+        const order = req.query.order === "asc" ? 1 : -1
+
+        // Query
+        const documents = await GeneralDocuments.find(filters)
+        .sort({ [sortBy]: order })
+        .skip(skip)
+        .limit(limit)
+
+        const totalDocuments = await GeneralDocuments.countDocuments(filters)
+
+        // Log Action
+        await ActivityLog.create({
+            userId,
+            type: "DOCUMENTS_FETCHED",
+            description: "User fetched all documents belonging to them",
+            ipAddress: req.ip,
+            deviceInfo: req.headers["user-agent"]
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "Documents fetched Successfully",
+            pagination: {
+                total: totalDocuments,
+                page,
+                limit,
+                totalPages: Math.ceil(totalDocuments/limit)
+            },
+            data: documents
+        })
+
+    } catch (error) {
+        console.error("Error while getting user documents: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while getting user documents"
+        })
+    }
+}
+
+// Controller to get all the documents (Admin)
+exports.getAllDocuments = async (req, res) => {
+    try {
+        // Ensuring only admins can use this
+        if(req.user.role !== "admin") {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden: Only admins can use this"
+            })
+        }
+
+        // Pagination
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 10
+        const skip = (page - 1) * limit
+
+        // Filtering
+        const filters = {}
+        if(req.query.type) filters.type = req.query.type
+
+        // Sorting
+        const sortBy = req.query.sortBy || "createdAt"
+        const order = req.query.order === "asc" ? 1 : -1
+
+        // Query
+        const documents = await GeneralDocuments.find(filters)
+        .populate("userId", "username email role")
+        .sort({ [sortBy]: order })
+        .skip(skip)
+        .limit(limit)
+
+        const totalDocuments = await GeneralDocuments.countDocuments(filters)
+
+        // Log Action
+        await ActivityLog.create({
+            userId: req.user.id,
+            type: "ADMIN_DOCUMENTS_FETCHED",
+            description: `Admin fetched all the documents (page: ${page}, limit: ${limit})`,
+            ipAddress: req.ip,
+            deviceInfo: req.headers["user-agent"]
+        })
+
+        return res.status(200).json({
+            success: true,
+            message: "Documents fetched Successfully",
+            pagination: {
+                total: totalDocuments,
+                page,
+                limit,
+                totalPages: Math.ceil(totalDocuments/limit)
+            },
+            data: documents
+        })
+
+    } catch (error) {
+        console.error("Error while getting all the docuemnts: ", error)
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong fetching all the documents"
+        })
     }
 }
