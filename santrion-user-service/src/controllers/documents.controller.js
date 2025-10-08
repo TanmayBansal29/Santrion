88d879fc5aa9f266ee1c5da95b553b908f47be3c
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-10-08 13:18:10 +0530

    add: upload document controller for users for uploading

--- santrion-user-service/src/controllers/documents.controller.js
+++ santrion-user-service/src/controllers/documents.controller.js
@@ -1,5 +1,5 @@
 const CloudinaryService = require("../services/cloudinaryService")
-
+const streamifier = require("streamifier")
 
 // controller to upload a document
 exports.uploadDocument = async (req, res) => {
@@ -38,16 +38,48 @@ exports.uploadDocument = async (req, res) => {
                     {
                         folder: `users/${userId}/documents`,
                         resource_type: "auto"
+                    },
+                    (error, result) => {
+                        if(error) reject (error)
+                        else resolve(result)
                     }
                 )
+                streamifier.createReadStream(req.file.buffer).pipe(stream)
             })
         }
+
+        const result = await uploadToCloudinary()
+
+        // Save document metadata
+        const document = await GeneralDocuments.create({
+            userId,
+            name: req.file.originalname,
+            type: type || "other",
+            url: result.secure_url,
+            fileSize: req.file.size,
+            fileFormat: req.file.mimetype,
+        });
+
+        // Log activity
+        await ActivityLog.create({
+            userId,
+            type: "DOCUMENT_UPLOADED",
+            description: `User uploaded a new ${document.type} document`,
+            ipAddress: req.ip,
+            deviceInfo: req.headers["user-agent"],
+        });
+
+        return res.status(200).json({
+            success: false,
+            message: "Document Uploaded successfully",
+            data: document
+        })
 
     } catch (error) {
         console.log("Error while uploading the document: ", error)
         return res.status(500).json({
             success: false,
             message: "Something went wrong uploading the document"
-        })
+        })  
     }
 }
